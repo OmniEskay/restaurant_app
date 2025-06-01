1f7217c1cbcba01b5a2bf008f520d3b2d9b89253
+ <omnieskay@proton.me>
2025-06-01 03:05:18 +0300

    data models

--- models/menu_item.py
+++ models/menu_item.py
@@ -3,10 +3,14 @@ from models import Base
 
 class MenuItem(Base):
     __tablename__ = 'menu_items'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False, unique=True)
     category = Column(String, nullable=False)
     price = Column(Float, nullable=False)
+
+    def __repr__(self):
+        return f"<MenuItem(id={self.id}, name='{self.name}', category='{self.category}', price={self.price})>"
 
     @property
     def is_valid(self):
--- models/order.py
+++ models/order.py
@@ -4,7 +4,12 @@ from models import Base
 
 class Order(Base):
     __tablename__ = 'orders'
+
     id = Column(Integer, primary_key=True)
     customer_name = Column(String, nullable=False)
     menu_item_id = Column(Integer, ForeignKey('menu_items.id'))
+
     menu_item = relationship("MenuItem", backref="orders")
+
+    def __repr__(self):
+        return f"<Order(id={self.id}, customer='{self.customer_name}', item='{self.menu_item.name}')>"
